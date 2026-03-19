0b508b45c20f204a0c6975e1abe6e8f38afa195a
sundaram <sundaramseth.ucer@gmail.com>
2026-03-19 23:22:44 +0530

    updates

--- api/gas.js
+++ api/gas.js
@@ -4,26 +4,31 @@ export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
-    if (req.method === "OPTIONS") {
+  if (req.method === "OPTIONS") {
     return res.status(200).end();
-     }
+  }
 
-    try {
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
 
+  try {
     const response = await fetch(
       "https://script.google.com/macros/s/AKfycbzQ7fHVzvAclAadfbi-DDvi2MF416wyvaSGrXFz8_JZ7lKtppQ77T0_nrEHd_Gapuir/exec",
       {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
         body: JSON.stringify(req.body),
       }
     );
 
     const text = await response.text();
 
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).send(text);
+    return res.status(200).send(text);
 
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    return res.status(500).json({ error: err.message });
   }
 }

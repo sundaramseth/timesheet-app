199a3a32303222a7a215011bbc42a2ca01a8162b
sundaram <sundaramseth.ucer@gmail.com>
2026-03-23 15:27:13 +0530

    updates vercel settings

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    historyApiFallback: true,
     proxy: {
       '/api/gas': {
         target: 'https://script.google.com',
